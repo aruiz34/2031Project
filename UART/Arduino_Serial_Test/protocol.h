bfd3722417fe37d915f7a03b60e3c89fab4a242a
Cory L <nnbrain8@gmail.com>
2017-11-14 13:12:38 -0500

    initial implementation of c-side api

--- UART/Arduino_Serial_Test/protocol.h
+++ UART/Arduino_Serial_Test/protocol.h
@@ -6,13 +6,13 @@
 uint16_t getSonar(uint8_t snum);
 uint16_t getOdometerX(void);
 uint16_t getOdometerY(void);
-uint16_t getOdometerTh(void);
 uint16_t resetOdometer(void);
 uint16_t forward(uint16_t speed);
 uint16_t turn(uint16_t angle);
 uint16_t stop(void);
 uint16_t beep(uint8_t len, uint8_t freq);
 uint16_t sonarEnable(uint8_t smask);
+uint16_t getOdometerTh(void);
 
 
 #endif
